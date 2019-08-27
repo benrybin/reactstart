1a4c7e25fe3484ffc79320084be55adc23e55ca3
benrybinski <benrybin@gmail.com>
2019-08-27 16:28:01 -0400

    issue

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,48 +6,47 @@ import './index.css';
 
 function Square(props){
     return (
-        <button className="square" onClick={ 
-        props.onClick}>
+        <button className="square" onClick=
+        {props.onClick}>
           {props.value}
         </button>
       );
     }
   
   
-  class Board extends React.Component {
+    class Board extends React.Component {
+        renderSquare(i) {
+          return (
+            <Square
+              value={this.props.squares[i]}
+              onClick={() => this.props.onClick(i)}
+            />
+          );
+        }
       
-    renderSquare(i) {
-      return( 
-      <Square 
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
-      />);
-    }
-  
-    render() {
-        
-      return (
-        <div>
-          
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
+        render() {
+          return (
+            <div>
+              <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+              </div>
+              <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+              </div>
+              <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+              </div>
+            </div>
+          );
+        }
+      }
+      
   
   class Game extends React.Component {
       constructor(props){
@@ -56,8 +55,16 @@ function Square(props){
               history: [{
                   squares: Array(9).fill(null),
               }],
+              stepNumber: 0,
               xIsNext: true,
           };
+      }
+      jumpTo(step){
+          this.setState({
+              stepNumber: step,
+              xIsNext: (step % 2) === 0,
+
+          });
       }
       handleClick(i) {
           const history = this.state.history;
@@ -72,12 +79,13 @@ function Square(props){
                   squares:squares,
 
               }]),
+              stepNumber: history.length,
               xIsNext: !this.state.xIsNext,
           });
       }
     render() {
         const history = this.state.history;
-        const current = history[history.length -1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         const moves = history.map((step,move) => {
             const desc = move ?
